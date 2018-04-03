7bd026569ef91a9f0a63a0146990f55e09f0a9d5
Lars <lars.tebooy@hotmail.com>
2018-04-03 09:58:15 +0200

    added repository and Crud annotations

--- src/main/java/nl/yacht/xyzairlines/controller/AirplaneController.java
+++ src/main/java/nl/yacht/xyzairlines/controller/AirplaneController.java
@@ -1,4 +1,41 @@
 package nl.yacht.xyzairlines.controller;
 
+import nl.yacht.xyzairlines.model.Airplane;
+import nl.yacht.xyzairlines.repository.IAirplaneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/airplanes/")
 public class AirplaneController {
+
+    @Autowired
+    private IAirplaneRepository airplaneRepository;
+
+    //add Airplane
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public Airplane create(@RequestBody Airplane airplane){
+        return this.airplaneRepository.save(airplane);
+    }
+
+    //Get all Airplanes
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public Iterable<Airplane> getAll(){
+        return this.airplaneRepository.findAll();
+    }
+
+    //Get one Airplane
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public Airplane getOne(@PathVariable long id){
+        return this.airplaneRepository.findOne(id);
+    }
+
+    //Delete Airplane
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable long id){
+        this.airplaneRepository.delete(id);
+    }
+
+
+
 }
--- src/main/java/nl/yacht/xyzairlines/repository/IAirplaneRepository.java
+++ src/main/java/nl/yacht/xyzairlines/repository/IAirplaneRepository.java
@@ -2,6 +2,8 @@ package nl.yacht.xyzairlines.repository;
 
 import nl.yacht.xyzairlines.model.Airplane;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IAirplaneRepository extends CrudRepository<Airplane, Long> {
 }
